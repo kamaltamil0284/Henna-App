cdc0a0468adcfd428dea7a48642eb989e37c3b7a
kamaltamil0284 <kamaltamil0284@gmail.com>
2025-10-01 00:19:11 +0530

    Update base files

--- HennaApp/urls.py
+++ HennaApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',Home,name='home'),
+    path('home/',Home,name='home'),
 ]
--- HennaArt/settings.py
+++ HennaArt/settings.py
@@ -122,6 +122,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATIC_VERSION =1
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
