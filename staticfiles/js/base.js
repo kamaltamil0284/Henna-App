620ea7ea099b956e191862c7a2adf87447c03da2
kamaltamil0284 <kamaltamil0284@gmail.com>
2025-10-01 13:13:33 +0530

    Project updated

---
+++ staticfiles/js/base.js
@@ -0,0 +1,12 @@
+const hamburger= document.querySelector(".hamburger");
+const navbar_nav= document.querySelector(".navbar-nav");
+
+hamburger.addEventListener("click",() => {
+    hamburger.classList.toggle("active")
+    navbar_nav.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navbar_nav.classList.remove("active");
+}))
